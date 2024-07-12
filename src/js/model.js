60173a00a1f06ff9e5bfeccac55e228daa0152c7
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-07-12 21:02:14 +0530

    Fixed a bug. Now page 1 gets displayed when new search is done instead a page we were in the previous search.

--- src/js/model.js
+++ src/js/model.js
@@ -42,6 +42,7 @@ export const loadSearchResults = async function (query) {
         image: recipe.image_url,
       };
     });
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err} 😱😱😱😱😱`);
     throw err;
