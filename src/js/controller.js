498cd5ef6d52986facc983601cdcd5998c500c1a
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-07-13 08:03:54 +0530

    Needs bookmark bug fix

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
+import bookmarksView from "./views/bookmarksView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -19,6 +20,7 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
 
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
     // * 1 . Loading Recipe
     await model.loadRecipe(id);
 
@@ -51,21 +53,24 @@ const controlPagination = function (goToPage) {
 
 const controlServings = function (newServings) {
   model.updateServings(newServings);
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
-const controlBookmark = function () {
+const controlUpdateBookmark = function () {
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.removeBookmark(model.state.recipe.id);
 
+  console.log(model.state.recipe.bookmarked);
+
   recipeView.update(model.state.recipe);
+
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
-  recipeView.addHandlerAddBookmark(controlBookmark);
+  recipeView.addHandlerAddBookmark(controlUpdateBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,17 @@
+import View from "./view.js";
+import previewView from "./previewView.js";
+import icons from "url:../../img/icons.svg";
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector(".bookmarks__list");
+  _errorMessage = "No bookmark yet. Find a nice recipe to bookmark!";
+  _message = "";
+
+  _generateMarkup() {
+    return this._data
+      .map((bookmark) => previewView.render(bookmark, false))
+      .join("");
+  }
+}
+
+export default new BookmarksView();
