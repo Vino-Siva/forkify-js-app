5673cfba37d17ce2551edc449a2623384f7e48f5
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-07-13 16:23:52 +0530

    Bug fix to remove bookmark

--- src/js/controller.js
+++ src/js/controller.js
@@ -59,11 +59,7 @@ const controlServings = function (newServings) {
 const controlUpdateBookmark = function () {
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.removeBookmark(model.state.recipe.id);
-
-  console.log(model.state.recipe.bookmarked);
-
   recipeView.update(model.state.recipe);
-
   bookmarksView.render(model.state.bookmarks);
 };
 
